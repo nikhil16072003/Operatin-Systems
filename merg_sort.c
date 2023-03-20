03a4f8ec409763fef45c7d6c19e47e61fa420659
Ambati Nikhil Reddy <97824578+nikhil16072003@users.noreply.github.com>
2023-03-20 19:41:03 +0530

    Create merg_sort.c

---
+++ merg_sort.c
@@ -0,0 +1,56 @@
+//Merge sort
+
+#include <stdio.h>
+ 
+
+void merge_sort(int i, int j, int a[], int aux[]) {
+    if (j <= i) {
+        return;     
+    }
+    int mid = (i + j) / 2;
+
+    
+    
+    merge_sort(i, mid, a, aux);     
+    merge_sort(mid + 1, j, a, aux);    
+
+    int pointer_left = i;       
+    int pointer_right = mid + 1;        
+    int k;      
+    for (k = i; k <= j; k++) {
+        if (pointer_left == mid + 1) {      
+            aux[k] = a[pointer_right];
+            pointer_right++;
+        } 
+        else if (pointer_right == j + 1) {        
+            aux[k] = a[pointer_left];
+            pointer_left++;
+        } 
+        else if (a[pointer_left] < a[pointer_right]) {        
+            aux[k] = a[pointer_left];
+            pointer_left++;
+        }
+        else {        
+            aux[k] = a[pointer_right];
+            pointer_right++;
+        }
+    }
+
+    for (k = i; k <= j; k++) {      
+        a[k] = aux[k];
+    }
+}
+
+
+int main() {
+    int n;
+    scanf("%d",&n);
+  int a[n], aux[100],  i, d, swap;
+  for (i = 0; i < n; i++)
+    scanf("%d", &a[i]);
+ 
+  merge_sort(0, n - 1, a, aux);
+  for (i = 0; i < n; i++)
+     printf("%d ", a[i]);
+     return 0;
+}
