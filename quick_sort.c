1c183806a73ee7b938de7d0e0cd95af48a52c44d
Ambati Nikhil Reddy <97824578+nikhil16072003@users.noreply.github.com>
2023-03-20 19:42:30 +0530

    Create quick_sort.c

---
+++ quick_sort.c
@@ -0,0 +1,47 @@
+//quick sort
+
+#include<stdio.h>
+void quicksort(int number[25],int first,int last){
+   int i, j, pivot, temp;
+   if(first<last){
+       pivot=first;
+        i=first;
+        j=last;
+       
+            while(number[i]<=number[pivot]&&i<last){
+                i++;
+            }
+            while(number[j]>number[pivot]){
+                j--;
+            }
+            if(i<j){
+                temp=number[i];
+                number[i]=number[j];
+                number[j]=temp;
+                
+            }
+            
+    
+temp=number[pivot];
+number[pivot]=number[j];
+number[j]=temp;
+quicksort(number,first,j-1);
+quicksort(number,j+1,last);
+       
+   }
+}
+
+int main(){
+int i, count;
+scanf("%d",&count);
+int number[count];
+for(i=0;i<count;i++){
+    scanf("%d",&number[i]);
+}
+quicksort(number,0,count-1);
+for(i=0;i<count;i++){
+    printf(" %d",number[i]);
+}
+return 0;
+    
+}
